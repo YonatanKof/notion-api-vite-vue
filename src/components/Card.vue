93dcd4d4f355e53b355f7908d330864517d9c982
Yonatan Kof <kof@yonatankof.com>
2022-10-31 20:13:09 +0200

    Add Card & favicon

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+	<article>
+		<h1 class="title">Connector</h1>
+		<img src="../assets/Connector.png" alt="flow chart item" />
+		<p>A jump from one point to another</p>
+	</article>
+</template>
+
+<style scoped>
+article {
+	background-color: bisque;
+    color: firebrick;
+    border-radius: 0.5rem;
+    width: 100%;
+}
+img {
+	max-width: 16rem;
+}
+.title{
+    font-size: 2rem;
+}
+</style>
