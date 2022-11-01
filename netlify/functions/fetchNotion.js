ca782763d715628c3142e62aeaba5391e9b6e527
Yonatan Kof <kof@yonatankof.com>
2022-11-01 13:15:51 +0200

    Add notion clinet and call data with rty catch!

--- netlify/functions/fetchNotion.js
+++ netlify/functions/fetchNotion.js
@@ -1,6 +1,35 @@
+const { Client } = require("@notionhq/client");
+
+// Call our the env keys
+const { NOTION_KEY, NOTION_DB } = process.env;
+
+// Initializing a client
+const notion = new Client({
+	// auth: process.env.NOTION_TOKEN,
+	auth: NOTION_KEY,
+});
+
 exports.handler = async function (event, context) {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Hello World" }),
-	};
+	try {
+		const response = await notion.databases.query({
+			database_id: NOTION_DB,
+			filter: {
+				property: "Live",
+				checkbox: {
+					equals: true,
+				},
+			},
+		});
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ response }),
+		};
+	} catch (e) {
+		console.error(e);
+		return {
+			statusCode: 500,
+			body: e.toString(),
+		};
+	}
 };
