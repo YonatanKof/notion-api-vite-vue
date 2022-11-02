c19fa2a3198b82655609167899d2aeb879785fec
Yonatan Kof <kof@yonatankof.com>
2022-11-02 10:04:27 +0200

    Minor renaming

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, ref } from "vue";
 
-const postData = ref([]);
+const itemData = ref([]);
 
 const getData = async () => {
 	return fetch("/api/fetchNotion").then((res) => res.json());
@@ -9,14 +9,14 @@ const getData = async () => {
 
 onMounted(() => {
 	getData().then((data) => {
-		postData.value = data.response.results;
+		itemData.value = data.response.results;
 	});
 });
 </script>
 
 <template>
 	<main>
-		<article v-for="item in postData">
+		<article v-for="item in itemData">
 			<h1 class="title">{{ item.properties.Name.title[0].plain_text }}</h1>
 			<img :src="item.properties.Image.files[0].file.url" :alt="item.properties.Alt.formula.string" />
 			<p>{{ item.properties.Description.rich_text[0].plain_text }}</p>
