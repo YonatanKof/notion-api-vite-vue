d98010b711c264a4cbe10d3ad3e015e2ff91c7f8
Yonatan Kof <kof@yonatankof.com>
2022-11-03 18:17:10 +0200

    Add router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../Pages/Home.vue'
+import Symbol from '../pages/Symbol.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/symbol', name: 'Symbol', component: Symbol }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
